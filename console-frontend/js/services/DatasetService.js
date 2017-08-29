14b8d0094c89ce891980ff3fb796a6a7bdd13335
nandhalabs <nandhab@users.noreply.github.com>
2017-08-30 01:24:24 +0530

    Update DatasetService.js

--- console-frontend/js/services/DatasetService.js
+++ console-frontend/js/services/DatasetService.js
@@ -1,7 +1,8 @@
 /*-------------------------------------------------------------------------------
 * Name:        DatasetService.js
 * Purpose:     Service for calling the backend datasets API.
-***
+*
+*
 * Author:      PNDA Team
 * Created:     2016/04/01
 * History:     2016/04/01 - Initial commit
